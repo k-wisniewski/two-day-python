0c7fc1972eadbafc91e52ceffdf5ecb563c58f68
Krzysztof Wiśniewski <krzysztof.wisniewski@bottega.com.pl>
2023-11-19 13:57:42 +0100

    Add solution for pydantic task about date range

--- examples/date_range.py
+++ examples/date_range.py
@@ -1,29 +1,53 @@
 from datetime import datetime, timedelta
-from typing import Self
+from typing import Annotated, Self
+
+from pydantic import BaseModel, Field, PositiveInt, model_validator, validate_call
 
 
+class DateRange(BaseModel):
+    start: datetime = Field(frozen=True)
+    end: datetime = Field(frozen=True)
 
-class DateRange:
-    start: datetime
-    end: datetime
-
-    def __init__(self: Self, start: datetime, end: datetime) -> None:
-        self.start = start
-        self.end = end
+    @model_validator(mode="after")
+    def validate_start_before_end(self: Self) -> Self:
+        if self.start > self.end:
+            raise ValueError("start datetime can't be before end datetime")
+        return self
 
     @classmethod
-    def from_last(cls, last_str: str) -> Self:
-        if "h" in last_str:
-            now = datetime.now()
-            return cls(start=now - timedelta(hours=_get_numeric_val_from_str(last_str, "h")), end=now)
-        elif "d" in last_str:
-            now = datetime.now()
-            return cls(start=now - timedelta(days=_get_numeric_val_from_str(last_str, "d")), end=now)
-        elif "m" in last_str:
-            now = datetime.now()
-            return cls(start=now - timedelta(days=30 * _get_numeric_val_from_str(last_str, "m")), end=now)
+    @validate_call
+    def years_back(cls, years: PositiveInt) -> Self:
         now = datetime.now()
-        return cls(start=now - timedelta(days=365 * _get_numeric_val_from_str(last_str, "y")), end=now)
+        return cls(start=now - timedelta(days=365 * years), end=now)
+
+    @classmethod
+    @validate_call
+    def months_back(cls, months: PositiveInt) -> Self:
+        now = datetime.now()
+        return cls(start=now - timedelta(days=30 * months), end=now)
+
+    @classmethod
+    @validate_call
+    def days_back(cls, days: PositiveInt) -> Self:
+        now = datetime.now()
+        return cls(start=now - timedelta(days=days), end=now)
+
+    @classmethod
+    @validate_call
+    def hours_back(cls, hours: PositiveInt) -> Self:
+        now = datetime.now()
+        return cls(start=now - timedelta(hours=hours), end=now)
+
+    @classmethod
+    @validate_call
+    def from_last(cls, last_str: Annotated[str, Field(pattern=r"^\d*[hdmy]$")]) -> Self:
+        if "h" in last_str:
+            return cls.hours_back(_get_numeric_val_from_str(last_str, "h"))
+        elif "d" in last_str:
+            return cls.days_back(_get_numeric_val_from_str(last_str, "d"))
+        elif "m" in last_str:
+            return cls.months_back(_get_numeric_val_from_str(last_str, "m"))
+        return cls.years_back(_get_numeric_val_from_str(last_str, "y"))
 
 
 def _get_numeric_val_from_str(last_str: str, delim: str) -> int:
