e3348cc26c8c805e0e2bdaa0391df9243927526c
Krzysztof Wiśniewski <krzysztof.wisniewski@bottega.com.pl>
2023-11-19 18:29:18 +0100

    Add solution for a task about caching decorator

--- examples/caching_decorator.py
+++ examples/caching_decorator.py
@@ -1,8 +1,25 @@
+from functools import wraps
 from time import sleep
 
 
 def cache(max_size=10):
-    ...
+    def decorator(f):
+        _cache = {}
+
+        def _ensure_space_left_in_cache():
+            if len(_cache) == max_size:
+                _cache.pop(next(iter(_cache)))
+        
+        @wraps(f)
+        def wrapper(*args, **kwargs):
+            cache_key = f"{f.__name__}_{'_'.join(map(str, args))}_{'_'.join(f'{k}={v}' for k, v in kwargs.items())}"
+
+            if cache_key not in _cache:
+                _ensure_space_left_in_cache()
+                _cache[cache_key] = f(*args, **kwargs)
+            return _cache[cache_key]
+        return wrapper
+    return decorator
 
 @cache(max_size=2)
 def slow_adder(x, y):
