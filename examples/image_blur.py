9c6cd1793beab218c15812ffc476f52d9d7cf6af
Krzysztof Wiśniewski <krzysztof.wisniewski@bottega.com.pl>
2023-11-19 13:51:55 +0100

    Add solution for a mypy task

--- examples/image_blur.py
+++ examples/image_blur.py
@@ -1,7 +1,8 @@
 import click
 from PIL import Image, ImageFilter, ImageEnhance
+from pathlib import Path
 
-def apply_gaussian_blur(image, blur_radius):
+def apply_gaussian_blur(image: Image.Image, blur_radius: float) -> Image.Image:
     """Applies Gaussian blur to an image."""
     return image.filter(ImageFilter.GaussianBlur(blur_radius))
 
@@ -9,36 +10,43 @@ def rotate_image(image: Image.Image) -> Image.Image:
     """Rotates the image 90 degrees clockwise."""
     return image.rotate(-90.0, expand=True)
 
-def apply_hdr_effect(image: Image.Image):
+def apply_hdr_effect(image: Image.Image) -> Image.Image:
     """Applies a simulated HDR effect to the image."""
     # Enhance color
-    enhancer = ImageEnhance.Color(image)
-    image = enhancer.enhance(1.5)
+    color_enhancer = ImageEnhance.Color(image)
+    image = color_enhancer.enhance(1.5)
 
     # Enhance contrast
-    enhancer = ImageEnhance.Contrast(image)
-    return enhancer.enhance(1.5)
+    contrast_enhancer = ImageEnhance.Contrast(image)
+    return contrast_enhancer.enhance(1.5)
 
 @click.command()
 @click.argument('image_path', type=click.Path(exists=True, dir_okay=False))
-@click.option('--blur', 'apply_blur', is_flag=True, help='Apply Gaussian blur to the image.')
-@click.option('--blur-radius', type=float, default=2.0, show_default=True, help='Radius for Gaussian blur.')
-@click.option('--rotate', 'rotate_image', is_flag=True, help='Rotate the image 90 degrees clockwise.')
-@click.option('--hdr', 'apply_hdr', is_flag=True, help='Apply simulated HDR effect to the image.')
-@click.option('--save-path', type=click.Path(), help='Path to save the processed image.', required=True)
-def main(image_path, apply_blur, blur_radius, rotate_image, apply_hdr, save_path):
+@click.option('--blur', 'should_apply_blur', is_flag=True, help='Apply Gaussian blur to the image.', type=click.BOOL)
+@click.option('--blur-radius', type=float, default=2.0, show_default=True, help='Radius for Gaussian blur.', type=click.FLOAT)
+@click.option('--rotate', 'should_rotate_image', is_flag=True, help='Rotate the image 90 degrees clockwise.', type=click.BOOL)
+@click.option('--hdr', 'should_apply_hdr', is_flag=True, help='Apply simulated HDR effect to the image.', type=click.BOOL)
+@click.option('--save-path', type=click.Path(dir_okay=False), help='Path to save the processed image.', required=True)
+def main(
+    image_path: Path,
+    should_apply_blur: bool,
+    blur_radius: float,
+    should_rotate_image: bool,
+    should_apply_hdr: bool,
+    save_path: Path
+) -> None:
     """
     Processes an IMAGE_PATH with optional effects: Gaussian blur, 90-degree rotation, and simulated HDR.
     """
     try:
         with Image.open(image_path) as img:
-            if apply_blur:
-                img = apply_gaussian_blur(blur_radius, img)
+            if should_apply_blur:
+                img = apply_gaussian_blur(img, blur_radius)
             
-            if rotate_image:
+            if should_rotate_image:
                 img = rotate_image(img)
             
-            if apply_hdr:
+            if should_apply_hdr:
                 img = apply_hdr_effect(img)
 
             img.save(save_path)
